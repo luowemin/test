20f675490b4beb180541153921e16b85ee694802
luowm <984755513@qq.com>
2017-03-29 11:16:59 +0800

    test

---
+++ build/build.js
@@ -0,0 +1,22 @@
+var webpack = require("webpack");
+var config = require('./webpack.config');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var html = require("html-webpack-plugin");
+var path = require("path");
+config.plugins = [
+	new html({
+		template:path.resolve(__dirname, "../src/html/productManager.ejs"),
+		filename:"productManager.html",
+	}),
+	new ExtractTextPlugin("[name]-[hash].css")
+]
+console.log(config.entry);
+webpack(config,function(err,stats){
+	if(err) throw err;
+	var jsonStats = stats.toJson();
+	if(jsonStats.errors.length>0){
+		console.error(jsonStats.errors);
+	}else{
+		console.log("success");
+	}
+});
